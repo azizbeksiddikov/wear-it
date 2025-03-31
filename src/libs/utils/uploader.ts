205ecbeb40fd723e3302d5e3c9ad6c1e62e92bef
Azizbek Siddikov <azbeksid@gmail.com>
2025-04-01 01:33:01 +0900

    refactor: comment unnecssary code

--- src/libs/utils/uploader.ts
+++ src/libs/utils/uploader.ts
@@ -6,30 +6,30 @@ import { supabase } from "./supabase";
 import { decode } from "base64-arraybuffer";
 import Errors, { HttpCode, Message } from "../Errors";
 
-function getTargetImageStorage(address: any) {
-  const targetPath = `./uploads/${address}`;
-  if (!fs.existsSync(targetPath)) {
-    fs.mkdirSync(targetPath, { recursive: true });
-  }
+// function getTargetImageStorage(address: any) {
+//   const targetPath = `./uploads/${address}`;
+//   if (!fs.existsSync(targetPath)) {
+//     fs.mkdirSync(targetPath, { recursive: true });
+//   }
 
-  return multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, targetPath);
-    },
-    filename: function (req, file, cb) {
-      const extension = path.parse(file.originalname).ext;
-      const random_name = v4() + extension;
-      cb(null, random_name);
-    },
-  });
-}
+//   return multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, targetPath);
+//     },
+//     filename: function (req, file, cb) {
+//       const extension = path.parse(file.originalname).ext;
+//       const random_name = v4() + extension;
+//       cb(null, random_name);
+//     },
+//   });
+// }
 
-const makeUploader = (address: string) => {
-  const storage = getTargetImageStorage(address);
-  return multer({ storage: storage });
-};
+// const makeUploader = (address: string) => {
+//   const storage = getTargetImageStorage(address);
+//   return multer({ storage: storage });
+// };
 
-export default makeUploader;
+// export default makeUploader;
 
 export const memoryUploader = multer({
   storage: multer.memoryStorage(), // Store files in memory as Buffer
